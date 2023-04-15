8860560be147a45eb5bff8eb566a5ae86dc266e5
raghvendra <raghvendraa.dev@gmail.com>
2023-04-15 15:49:28 +0530

    nuxt setup

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto space-y-2">
+      <div>
+        <h1 class="text-2xl">Home page</h1>
+        <p class="text-lg">I'm learning about Nuxt 3! Here's what I've achieved so far:</p>
+      </div>
+    </div>  
+  </template>
